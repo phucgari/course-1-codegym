0bb6027cc6ce90d2e6e86aca7be8da24a188e1fa
phucgari <phucgari@gmail.com>
2022-12-09 12:35:43 +0700

    donectoftoc

--- webstorm/js2/script1.js
+++ webstorm/js2/script1.js
@@ -3,5 +3,13 @@ function calculate() {
     let chemgrade=+document.getElementById("chem").value;
     let biograde=+document.getElementById("bio").value;
     let average= (physgrade+chemgrade+biograde)/3;
-    alert("average grade is: "+ average)
+    alert("average grade is: "+ average);
+}
+function celtofah() {
+    let fahcalced=((+document.getElementById("Cel").value)*9/5)+32;
+    alert("your result is: " + fahcalced + " Farenheit")
+}
+function fahtocel() {
+    let celcalced=((+document.getElementById("Fah").value)-32)*5/9;
+    alert("your result is: " + celcalced + " Celsius")
 }
